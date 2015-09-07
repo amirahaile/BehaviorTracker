c5098cc3f5a4cf41779d4456b541e3370eef61fa
amirahaile <amira.dhaile@gmail.com>
2015-09-07 10:42:49 -0700

    Created register page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  before_action :require_login, except: [:index]
+  before_action :require_login, except: [:index, :new, :create]
 
   def index; end
+
+  def new
+    @user = User.new
+    flash[:notice] = "You're already logged in." if session[:user_id]
+  end
 end
