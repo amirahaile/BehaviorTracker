6ce2911afc05b659592ad7a94ad89db1a943f84f
amirahaile <amira.dhaile@gmail.com>
2015-09-07 17:35:32 -0700

    Created location & user factories.

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :location do
-    
+    latitude 48.86770
+    longitude 2.35092
   end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name "Rose"
+    last_name "Nylund"
+    email "betty@goldengirls.com"
+    password "i<3stOlaf"
+    password_confirmation "i<3stOlaf"
   end
-
 end
