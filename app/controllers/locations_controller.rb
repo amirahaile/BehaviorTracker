24772c20f77c1d32465ffcf15ae44de26b3602ab
amirahaile <amira.dhaile@gmail.com>
2015-09-08 16:49:19 -0700

    Saves the geo coordinates to a Location instance.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,15 @@
 class LocationsController < ApplicationController
+
+  def new; end
+
+  def create
+    location = Location.create(latitude: params[:latitude],
+                               longitude: params[:longitude])
+    user = User.find(session[:user_id])
+    user.locations << location
+
+    # 500 error - missing template locations#create
+    # is 'render' right though?
+    render :new
+  end
 end
